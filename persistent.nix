0648a683c09afdd64dcc68c1ea566f5ee59fa832
CCH <126532936+changhwanchoe@users.noreply.github.com>
2026-07-07 00:09:07 +0900

    Update persistent.nix

--- persistent.nix
+++ persistent.nix
@@ -3,31 +3,32 @@
     enable = true;
     hideMounts = true;
     directories = [
-      "/var/log"
-      "/var/lib/bluetooth"
+      # "/var/log"
+      # "/var/lib/bluetooth"
       "/var/lib/nixos" # necessary
-      "/var/lib/systemd/coredump" # necessary
+      # "/var/lib/systemd/coredump"
+      # { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
     ];
     files = [
       "/etc/machine-id"
-      { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+      # { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
     ];
     users.cch = {
       directories = [
         "Downloads"
-        "Music"
-        "Pictures"
+        # "Music"
+        # "Pictures"
         "Documents"
-        "Videos"
-        "VirtualBox VMs"
-        { directory = ".gnupg"; mode = "0700"; }
-        { directory = ".ssh"; mode = "0700"; }
-        { directory = ".nixops"; mode = "0700"; }
-        { directory = ".local/share/keyrings"; mode = "0700"; }
-        ".local/share/direnv"
+        # "Videos"
+        # "VirtualBox VMs"
+        # { directory = ".gnupg"; mode = "0700"; }
+        # { directory = ".ssh"; mode = "0700"; }
+        # { directory = ".nixops"; mode = "0700"; }
+        # { directory = ".local/share/keyrings"; mode = "0700"; }
+        # ".local/share/direnv"
       ];
       files = [
-        ".screenrc"
+        # ".screenrc"
       ];
     };
   };
