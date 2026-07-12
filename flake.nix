e02633286ba305d7288607419eda25988035435c
CCH <126532936+changhwanchoe@users.noreply.github.com>
2026-07-12 21:23:33 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,6 @@
     };
   };
   outputs = inputs@{ self, nixpkgs, home-manager, disko, impermanence, ... }: {
-    diskoConfigurations.disko = import ./disko.nix;
     # NOTE: 'nixos' is the default hostname
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [
