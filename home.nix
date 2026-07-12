45f4ac70a280e24ea2c1a2767fe83fe300ed1bbf
CCH <126532936+changhwanchoe@users.noreply.github.com>
2026-07-12 23:44:42 +0900

    Update home.nix

--- home.nix
+++ home.nix
@@ -32,6 +32,18 @@
       evil-collection
       magit
     ];
+    extraConfig = ''
+      (use-package evil
+        :init
+        (setq evil-want-keybinding nil)
+        :config
+        (evil-mode 1))
+
+      (use-package evil-collection
+        :after evil
+        :config
+        (evil-collection-init))
+    '';
   };
 
   wayland.windowManager.sway = {
